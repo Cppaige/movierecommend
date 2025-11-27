321f578b85a87bc5b21e6a2dd2427e5d53a470aa
2205223390 <2205223390@qq.com>
2025-11-27 15:10:59 +0800

    新增配置文件、样式文件和海报图片
    - 添加 local.config.js 配置文件
    - 新增 rating-page.css 评分页面样式
    - 添加 posters 海报图片目录

---
+++ local.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  mysql: {
+    user: 'scau11',          // 你的MySQL用户名
+    password: '202325330111',   // 你的MySQL密码
+    socketPath: '/var/lib/mysql/mysql.sock'  // 你的MySQL实际socket路径
+  }
+};
